df780ecde03c5d3598c272f58fa6b68ee7fc3ffc
Maggie Yang <43491401+maggiey22@users.noreply.github.com>
2020-04-02 14:58:21 -0700

    echo html

--- useful.py
+++ useful.py
@@ -18,3 +18,12 @@ def commentOut():
         result.append("#" + l)
     result = '\n'.join(result)
     pyperclip.copy(result)
+
+def echoHTML():
+    text = str(pyperclip.paste())
+    lines = text.split('\n')
+    result = []
+    for l in lines:
+        result.append("echo \'" + l + "';")
+    result = '\n'.join(result)
+    pyperclip.copy(result)
